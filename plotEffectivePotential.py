35a235c810e74340e8f090a5cd96137b02a3a27a
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2016-10-17 19:43:59 +0900

    stringの数変更

---
+++ plotEffectivePotential.py
@@ -0,0 +1,28 @@
+import makeInputFile as makeFile
+import subprocess
+import matplotlib.pyplot as plt
+
+freeEnergyZeropoint = 75.98 - 5.582
+f = open("interpNewData.dat","r")
+x = []
+y = []
+while True:
+    readLine = f.readline()
+    if readLine == "":
+        break
+    else:
+        a,b,c = map(float,readLine.split())
+        x.append((b-a) - (c-b))
+        makeFile.makeInputFile(b-a, c-b)
+        subprocess.call(["zsh", "start_program.sh"])
+        fileFreeEnergy = open("freeEnergy.dat", "r")
+        freeEnergy = float(fileFreeEnergy.read()) + freeEnergyZeropoint
+        fileFreeEnergy.close()
+        y.append(freeEnergy)
+
+plt.plot(x,y,"o")
+plt.plot(x,y,linewidth = 1,color = "m")
+plt.savefig("plotEffectivePotential.png")
+plt.show()
+
+f.close()
